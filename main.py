c38f21a3be68c2c31827241c4786784125fc3617
Timothy Ng <timnjj@outlook.com>
2014-04-29 13:08:43 +0800

    Removed redundant imports

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import praw, re, time, datetime, urllib2, sys, os, random
+import praw, re, datetime, urllib2, random
 
 # @([\w-]+[LHG]) : regex for low, high, G rank carves. Syntax: @dire-miralis-G / @barioth-L
 
